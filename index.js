483dfb0284a876a5243f39b161a5f6510ba27372
Nissrine Canina <niss.canina@gmail.com>
2021-10-06 10:18:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
-}
+  static capitalize(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9\s-' ]/g, "");
+  }
+  static titleize(string) {
+    let words = string.toLowerCase().split(" ")
+    
+  for (let i=0; i<words.length; i++) {
+      
+       words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+     
+         
+    }
+     return words.join(" ");
+  }
+ }
+
+      
+       
+      
